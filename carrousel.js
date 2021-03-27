40861398ed32342093b83cae102edd9da5824376
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-03-26 22:58:47 -0600

    Add carrousel

--- carrousel.js
+++ carrousel.js
@@ -10,17 +10,21 @@ class Carrousel{
     activeIndex = 0;
     constructor(element){
         this.dom = element;
-        this.children = [...this.dom.children];
+        this.children = [...this.dom.children].filter(e=>e.nodeName!='BUTTON');
+        console.log(this.children);
         this.dom.addEventListener('scroll', ()=>{
             this.children.forEach((child, i) => {
-                if(isElementVisible(child)){
+                if(this.isElementVisible(child)){
                     child.classList.add('visible');
                     this.activeIndex = i;
                 }else{
                     child.classList.remove('visible');
                 }
             });
+            console.log(this.children.map(c=>this.isElementVisible(c)), this.activeIndex)
         });
+        this.dom.querySelector('.carrousel-prev').addEventListener('click', this.goToPrev.bind(this));
+        this.dom.querySelector('.carrousel-next').addEventListener('click', this.goToNext.bind(this));
     }
     /**
      * @param {HTMLElement} element;
@@ -33,22 +37,24 @@ class Carrousel{
             top:    elementRect.top - parentRect.top,
             bottom: parentRect.bottom - elementRect.bottom,
             left:   elementRect.left - parentRect.left,
-            right:  parentRect.right - elementRect.right
+            right:  - parentRect.right + elementRect.right
         }
-        return Object.values(rect).every(item=>item>=0);
+        // console.log(rect)
+        return Object.values(rect).every(item=>item==0);
     }
     goToPrev(){
+        console.log(this.activeIndex)
         if(this.activeIndex==0){
-            this.children[this.children.length-1].scrollIntoView(true);
+            this.dom.scrollLeft = this.children[this.children.length-1].offsetLeft;
         } else {
-            this.children[this.activeIndex-1].scrollIntoView(true);
+            this.dom.scrollLeft = this.children[this.activeIndex-1].offsetLeft;
         }
     }
     goToNext(){
         if(this.activeIndex==this.children.length-1){
-            this.children[0].scrollIntoView(true);
+            this.dom.scrollLeft = this.children[0].offsetLeft;
         } else {
-            this.children[this.activeIndex+1].scrollIntoView(true);
+            this.dom.scrollLeft = this.children[this.activeIndex+1].offsetLeft;
         }
     }
 }
